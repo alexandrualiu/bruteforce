b5eb35d4ad5e3ab95019cb4ac56a492a48411cab
Alexandru Aliu <aal@novicell.no>
2020-09-28 17:11:22 +0200

    Fixed the multithreading

--- src/main.py
+++ src/main.py
@@ -2,18 +2,24 @@ from services.passgen import PassGenerator
 from services.repo import FileRepository
 from services.passwordcheker import PasswordChecker
 
+import concurrent.futures
 import string
 
-xls_path = 'C:\\Users\\alexa\\Documents\\Data\\Work\\BruteForce\\xlsx'
+
+xls_path = 'C:\\@Work\\P\\GitHub\\bruteforce\\src\\xlsx'
 xls_name = 'test.xlsx'
 
 pass_service = PassGenerator(string.ascii_letters+string.punctuation+'0123456789')
-file_service = FileRepository('C:\\Users\\alexa\\Documents\\Data\\Work\\BruteForce\\data')
+file_service = FileRepository('C:\\@Work\\P\\GitHub\\bruteforce\\src\\data')
 chek_service = PasswordChecker()
+
+def executie(p):
+    return chek_service.check_password(xls_path,xls_name,p)
 
 cmd_gen_pass = False
 cmd_try_pass = True
 cmd_pass_size = 3
+cmd_thread_number = 1
 
 if cmd_gen_pass:
     file_service.remove_all_files()
@@ -22,11 +28,14 @@ if cmd_gen_pass:
         file_service.add_password(('%d.txt' %(cmd_pass_size)), new_pass)
 
 if cmd_try_pass:
-    loaded_pass = file_service.load_file(('%d.txt' %(cmd_pass_size)))
-    for item in loaded_pass:
-        result = chek_service.check_password(xls_path, xls_name, item)
-        if result:
-            print (item)
-            break
+    loaded_pass = list(file_service.load_file(('%d.txt' %(cmd_pass_size))))
+    for page in range(0,len(loaded_pass),cmd_thread_number):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = []
+            for p in loaded_pass[page:page+cmd_thread_number]:
+                futures.append(executor.submit(executie,p))
 
+            for f in concurrent.futures.as_completed(futures):
+                print(f.result())
+ 
 print('Terminat')
--- src/services/passwordcheker.py
+++ src/services/passwordcheker.py
@@ -1,15 +1,41 @@
-import win32com.client
+import pythoncom, win32com.client, threading
 
-class PasswordChecker:
-    xl_app = win32com.client.Dispatch('Excel.Application')
-    
+class PasswordChecker():
+    xls_stream = None
+
+    def __init__(self):
+        pythoncom.CoInitialize()
+        xl = win32com.client.DispatchEx('Excel.Application')
+        self.xls_stream = pythoncom.CreateStreamOnHGlobal()   
+         
+        pythoncom.CoMarshalInterface(self.xls_stream, 
+                                    pythoncom.IID_IDispatch, 
+                                    xl._oleobj_, 
+                                    pythoncom.MSHCTX_LOCAL, 
+                                    pythoncom.MSHLFLAGS_TABLESTRONG) 
+
+        xl = None
+
+    def __del__(self):
+        self.xls_stream.Seek(0,0)
+        pythoncom.CoReleaseMarshalData(self.xls_stream)
+        self.xls_stream = None
+        pythoncom.CoUninitialize()
+
     def check_password(self, path, name, password):
+        pythoncom.CoInitialize()
+
+        self.xls_stream.Seek(0,0)
+        
+        myUnmarshaledInterface = pythoncom.CoUnmarshalInterface(self.xls_stream, pythoncom.IID_IDispatch)    
+        xl = win32com.client.Dispatch(myUnmarshaledInterface)
+
         try:
-            xl_wb = self.xl_app.Workbooks.Open('%s\\%s' %(path,name), False, True, None, password)
+            xl_wb = xl.Workbooks.Open('%s\\%s' %(path,name), False, True, None, password)
             if xl_wb.Name:
                 xl_wb.Close(False)
                 return True
         except:
-            return False      
+            return False
 
-    
+        return False
